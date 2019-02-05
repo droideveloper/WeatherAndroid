53fb5e47bb7e0611b14d8c0af6716a5ba1f231b2
droideveloper <fatih.sen.boun@gmail.com>
2019-02-05 17:04:11 +0300

    repo concept imp

---
+++ app/src/main/java/org/fs/weather/common/repo/RemoteForecastRepository.kt
@@ -0,0 +1,26 @@
+/*
+ * Weather Kotlin Android Copyright (C) 2019 Fatih, Ozan Inc..
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fs.weather.common.repo
+
+import io.reactivex.Observable
+import org.fs.weather.model.entity.Forecast
+import org.fs.weather.model.net.Resource
+
+interface RemoteForecastRepository {
+
+  fun loadForecastFor(q: String): Observable<Resource<Forecast>>
+}
